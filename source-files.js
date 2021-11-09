91c4ab3926f3843d08aeba062bd067190f1d5a7c
hhirtz <hhirtz@users.noreply.github.com>
2021-11-09 13:54:06 +0000

    deploy: 475b5b59201735500a91e165e13f41fdefe07883

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["metis"] = {"name":"","files":["lib.rs","option.rs"]};
+sourcesIndex["metis_sys"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
